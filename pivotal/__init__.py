051a5b81a8df2fc6787fe44e9740ddd5633e9913
Tomas Roun <tomas.roun8@gmail.com>
2023-07-06 21:33:03 +0200

    Bump version

--- pivotal/__init__.py
+++ pivotal/__init__.py
@@ -2,5 +2,5 @@ from pivotal.api import Variable, maximize, minimize
 from pivotal.errors import Infeasible, Unbounded
 
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __all__ = ["Variable", "minimize", "maximize", "Infeasible", "Unbounded"]
