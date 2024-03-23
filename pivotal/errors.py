32996f5060c8a02bd07c29680cd48d248c1c9fe8
Tomas Roun <tomas.roun8@gmail.com>
2024-03-23 22:28:50 +0100

    Refactor code

--- pivotal/errors.py
+++ pivotal/errors.py
@@ -1,16 +1,6 @@
-
-
 class Unbounded(Exception):
     pass
 
 
 class Infeasible(Exception):
-    pass
-
-
-class InvalidConstraint(Exception):
-    pass
-
-
-class InvalidObjective(Exception):
     pass
